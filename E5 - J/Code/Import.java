50df94668393af8239ef50cda839bf27a1e85f60
Danilo Pianini <danilo.pianini@unibo.it>
2013-04-09 12:03:25 +0200

    Added an example class with import java.utils.*

---
+++ E5 - J/Code/Import.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Import {
+
+  public static void main(String[] a) {
+    System.out.println("I'm using Java libraries to print: " + Arrays.toString(new int[]{0,1,2,3,4,5,6}));
+  }
+  
+}
