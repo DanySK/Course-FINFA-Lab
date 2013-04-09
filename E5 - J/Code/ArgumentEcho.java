32131a928a329615de6ea7c680d776a7a5f8fa27
Danilo Pianini <danilo.pianini@unibo.it>
2013-04-09 11:50:18 +0200

    Added E5

---
+++ E5 - J/Code/ArgumentEcho.java
@@ -0,0 +1,9 @@
+class ArgumentEcho {
+
+  public static void main(String[] a) {
+    for(int i = 0; i < a.length; i++) {
+      System.out.println("Argument nr. " + i + ": " + a[i]);
+    }
+  }
+  
+}
