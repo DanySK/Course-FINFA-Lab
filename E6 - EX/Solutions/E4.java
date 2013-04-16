2559f4248a7e609e623cc3938f8f1ee95ba599cb
Danilo Pianini <danilo.pianini@unibo.it>
2013-04-16 16:04:56 +0200

    Added E4 solution

--- E6 - EX/Solutions/E4.java
+++ E6 - EX/Solutions/E4.java
@@ -1,56 +1,60 @@
-class E4{
+class E4 {
 
-    /** Realizzare la funzione mat qui sotto riportata che dato in ingresso una matrice 
-      * nxn di nome m rappresentata come array di array (dove m[i] rappresenta l'i-esima
-      * riga), azzera la metà della matrice sopra la diagonale secondaria.
-      * Ad esempio, la matrice
-      *
-      *     3333
-      *     0313
-      *     1233
-      *     1123
-      *
-      * Deve essere trasformata in
-      *
-      *     0003
-      *     0013
-      *     0233
-      *     1123
-      *
-      *
-      * Si consideri anche che l'esecuzione del test (eseguita dal metodo main) deve riportare 
-      * il risultato true. Come ausilio per il debugging, si noti che la funzionalità arrayEquals
-      * sotto riportata stampa i due array che sta confrontando!
-      **/
-
-	public static int[][] mat(int[][] m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m.length; j++) {
-				if (i < m.length - 1 - j) {
-					m[i][j] = 0;
-				}
-			}
-		}
-		return m;
-	}
+	/*
+	 * Realizzare la funzione int[][] mat(int, int) che, dati due interi
+	 * rappresentanti rispettivamente la dimensione della matrice ed un valore,
+	 * crea una matrice quadrata grande quanto indicato i cui valori sono tutti
+	 * zero eccetto le due diagonali, che devono avere come valore quello del
+	 * secondo argomento.
+	 * 
+	 * Esempi:
+	 * 
+	 * mat(1,2)
+	 * 2
+	 * 
+	 * mat(2,0)
+	 * 0 0
+	 * 0 0
+	 * 
+	 * mat(3,1)
+	 * 1 0 1
+	 * 0 1 0
+	 * 1 0 1
+	 * 
+	 * mat(4,8)
+	 * 8 0 0 8
+	 * 0 8 8 0
+	 * 0 8 8 0
+	 * 8 0 0 8
+	 * 
+	 * 
+	 * Si consideri anche che l'esecuzione del test (eseguita dal metodo main)
+	 * deve riportare il risultato true. Per aiutarsi con questo esercizio,
+	 * viene fornito un esercizio analogo risolto nel file Example.java
+	 */
       
-    public static boolean test(){
-            int[][] m=new int[][]{
-                        new int[]{3,3,3,3,3},
-                        new int[]{0,3,1,1,3},
-                        new int[]{1,2,3,3,1},
-                        new int[]{0,1,2,3,1},
-                        new int[]{1,1,2,3,0}
-            };
-            int[][] m2=mat(m);
-            return 
-                arrayEquals(m2[0],new int[]{0,0,0,0,3}) &&
-                arrayEquals(m2[1],new int[]{0,0,0,1,3}) &&
-                arrayEquals(m2[2],new int[]{0,0,3,3,1}) &&
-                arrayEquals(m2[3],new int[]{0,1,2,3,1}) &&
-                arrayEquals(m2[4],new int[]{1,1,2,3,0});
-    }
-    
+	public static int[][] mat(int size, int n) {
+		int[][] res = new int[size][size];
+		for(int i = 0; i< size; i++){
+			res[i][i] = n;
+			res[i][size-1-i] = n;
+		}
+		return res;
+	}
+
+	public static boolean test() {
+		int[][] m2 = mat(5, 6);
+		return arrayEquals(mat(1, 0)[0], new int[] { 0 })
+				&& arrayEquals(mat(1, 1)[0], new int[] { 1 })
+				&& arrayEquals(mat(2, 1)[0], new int[] { 1, 1 })
+				&& arrayEquals(mat(2, 1)[1], new int[] { 1, 1 })
+				&& arrayEquals(m2[0], new int[] { 6, 0, 0, 0, 6 })
+				&& arrayEquals(m2[1], new int[] { 0, 6, 0, 6, 0 })
+				&& arrayEquals(m2[2], new int[] { 0, 0, 6, 0, 0 })
+				&& arrayEquals(m2[3], new int[] { 0, 6, 0, 6, 0 })
+				&& arrayEquals(m2[4], new int[] { 6, 0, 0, 0, 6 });
+	}
+
 	public static void main(String[] s) {
 		System.out.println("" + test());
 	}
