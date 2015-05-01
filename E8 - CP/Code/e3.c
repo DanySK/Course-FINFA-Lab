17f9ad1db4fce1ef476df7aa1764cb445503b9a0
Danilo Pianini <danilo.pianini@unibo.it>
2015-05-01 23:35:42 +0200

    Moar spacing fixed

--- E8 - CP/Code/e3.c
+++ E8 - CP/Code/e3.c
@@ -11,48 +11,48 @@
  il risultato true (1 nel caso specifico del C). 
 */
 
-int f(int a[],int size){
+int f(int a[], int size) {
 	return FALSE;
 }
     
     
 /* test deve restituire 1 (TRUE)*/
-int test(void){
+int test(void) {
 	
-	int a[] = {0,2};
-	int b[] = {0,1,1,1,1,2};
-	int c[] = {0,1,2};
-	int d[] = {0,1,1,2};
+	int a[] = {0, 2};
+	int b[] = {0, 1, 1, 1, 1, 2};
+	int c[] = {0, 1, 2};
+	int d[] = {0, 1, 1, 2};
 	
 	int s[] = {0};
 	int g[] = {1};
-	int h[] = {0,1};
+	int h[] = {0, 1};
 	int i[] = {2};
-	int l[] = {0,2,2};
-	int m[] = {0,1,1,1};
-	int e[] = {3,0,1,1,2,0,1,1,2};
-	int n[] = {0,1,1,2,0,1,1,2,3};	
+	int l[] = {0, 2, 2};
+	int m[] = {0, 1, 1, 1};
+	int e[] = {3, 0, 1, 1, 2, 0, 1, 1, 2};
+	int n[] = {0, 1, 1, 2, 0, 1, 1, 2, 3};	
 	int v[0];
 	
 	 
-	return f(a,2)  && 
-	f(b,6)  &&
-	f(c,3)  &&
-	f(d,4)  &&
-	!f(v,0) &&
-	!f(s,1) &&
-	!f(g,1) &&
-	!f(h,2) &&
-	!f(i,1) &&
-	!f(l,3) &&
-	!f(m,4) &&
-	!f(e,9) &&
-	!f(n,9);		
+	return f(a, 2)  && 
+	f(b, 6)  &&
+	f(c, 3)  &&
+	f(d, 4)  &&
+	!f(v, 0) &&
+	!f(s, 1) &&
+	!f(g, 1) &&
+	!f(h, 2) &&
+	!f(i, 1) &&
+	!f(l, 3) &&
+	!f(m, 4) &&
+	!f(e, 9) &&
+	!f(n, 9);		
 }
 
     
-int main(void){
-	printf("%d\n",test());
+int main(void) {
+	printf("%d\n", test());
 }
     
     
