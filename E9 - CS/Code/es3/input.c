0680c585ec2277cdb6abc48bca1060fc9d460b31
Danilo Pianini <danilo.pianini@unibo.it>
2015-05-12 12:23:18 +0200

    format with three spaces

--- E9 - CS/Code/es3/input.c
+++ E9 - CS/Code/es3/input.c
@@ -3,13 +3,13 @@
 #include "point.h"
 
 int main(int argc, char **argv) {
-  if(argc < 3) {
-    printf("This program requires at least two parameters. Terminating.\n");
-    return 1;
-  }
-  Point2D p;
-  p.x = atof(argv[1]);
-  p.y = atof(argv[2]);
-  printf("Created a new Point2D: (%f, %f))\n", p.x, p.y);
-  return 0;
+   if(argc < 3) {
+      printf("This program requires at least two parameters. Terminating.\n");
+      return 1;
+   }
+   Point2D p;
+   p.x = atof(argv[1]);
+   p.y = atof(argv[2]);
+   printf("Created a new Point2D: (%f, %f))\n", p.x, p.y);
+   return 0;
 }
