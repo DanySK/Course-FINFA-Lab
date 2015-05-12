12b22832dc01b4821f393f89fad59e1419d4eb21
Danilo Pianini <danilo.pianini@unibo.it>
2015-05-12 15:30:35 +0200

    Fix style in es2 (import from solutions)

--- E9 - CS/Code/es2/list.h
+++ E9 - CS/Code/es2/list.h
@@ -10,22 +10,24 @@ struct list {
 
 typedef struct list List;
 
-List *list_cons(int head,List *tail);
+List *list_cons(int head, List *tail);
 
 List *list_nil(void);
 
 int list_is_empty(List *l);
 
-List *list_from_array(int a[],int size);
+List *list_from_array(int a[], int size);
 
 int list_length(List *l);
 
-void list_append_to(List *l1,List *l2);
+void list_append_to(List *l1, List *l2);
 
 int list_search(List *l, int i);
 
 List *list_get(List *l, int i);
 
 char *list_to_string(List *l);
+
+void list_cut_zero(List *l);
 
 #endif
